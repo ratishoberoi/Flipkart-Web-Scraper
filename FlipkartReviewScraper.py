b2bb9d6a802308e7e9e684433bc2a21c31995694
RATISH3993 <111753745+RATISH3993@users.noreply.github.com>
2022-11-11 17:16:09 +0530

    Add files via upload

---
+++ FlipkartReviewScraper.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup as bs
+import requests as req
+import pandas as pd
+
+def appendMod(var,column):
+    if var is not None:
+        column.append(var.text)
+    else:
+        column.append("NA")
+
+link = (input("Enter the link of the desired product: "))
+page = req.get(link)
+page.content
+soup = bs(page.content, 'html.parser')
+soup.prettify()
+consumer=[]
+rating=[]
+heading=[]
+review=[]
+count=0
+
+for data in soup.findAll('div',class_='_16PBlm'):
+    names=data.find('p', attrs={'class':'_2sc7ZR _2V5EHH'})
+    stars=data.find('div', attrs={'class':'_3LWZlK _1BLPMq'})
+    head=data.find('p', attrs={'class':'_2-N8zT'})
+    rev=data.find('div', attrs={'class':'t-ZTKy'})
+    appendMod(names,consumer)
+    appendMod(stars,rating)
+    appendMod(head,heading)
+    appendMod(rev,review)
+    count+=1
+
+df=pd.DataFrame({'Consumer Name':consumer, 'Rating (Out of 5)':rating, 'Heading':heading, 'Review':review})
+df.head(count)
+df.to_csv("c:/Users/HP/OneDrive/Desktop/FlipkartWebScraper/Reviews.csv",index=False)
